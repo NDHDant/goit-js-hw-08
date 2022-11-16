0ded5459a1aec211822aa4fcb726a0e6d49143f0
Yaroslav Nedohoda <y.nedogoda@gmail.com>
2022-11-16 22:22:11 +0200

    add task 2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,18 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iFrame = document.querySelector('iframe');
+const player = new Player(iFrame);
+
+player.on('timeupdate', throttle(onTimedUpdate, 1000));
+
+function onTimedUpdate(e) {
+    
+    localStorage.setItem('videoplayer-current-time', e.seconds);
+};
+
+const getTimeSum = (localStorage.getItem('videoplayer-current-time'));
+if (getTimeSum !== null) {
+    player.setCurrentTime(getTimeSum)
+
+}
